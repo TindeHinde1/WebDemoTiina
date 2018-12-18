b054e3e0c774ee3fd51438beee246a0f3610ad8d
Etunimi SukunimiTiina <tiinah@kotiposti.net>
2018-12-18 13:56:55 +0200

    APIjuttuja ja oman demon korjailua

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/APIController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreBackend.Models;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/[controller]")] //tämä juttelee esim c# kanssa
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        [Route("merkkijono")]
+        public string Merkkijono()
+        {
+            return "Terve, Joensuu!";
+        }
+        [Route("päivämäärä")]
+        public DateTime Päivämäärä()
+        {
+            return DateTime.Now;
+        }
+        [Route("olio")]
+        public Henkilö Olio()
+        {
+            return new Henkilö()
+            {
+                Nimi = "Teppo Testaaja",
+                Osoite = "Kotikatu 12 B 8",
+                Ikä = 35
+            };
+        }
+        [Route("oliot")]
+        public List<Henkilö> Oliot()
+        {
+            List<Henkilö> henkilöt = new List<Henkilö>()
+            {
+                new Henkilö()
+                {
+                    Nimi = "Teppo Testaaja",
+                    Osoite = "Kotikatu 12 B 8",
+                    Ikä = 35
+                },
+                new Henkilö()
+                {
+                    Nimi = "Teija Testaaja",
+                    Osoite = "Kotikatu 12 B 8",
+                    Ikä = 34
+                },
+                new Henkilö()
+                {
+                    Nimi = "Matti Mekaanikko",
+                    Osoite = "Mekaniikkatie 61",
+                    Ikä = 45
+                }
+            };
+            return henkilöt;
+        }
+    }
+}
---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/Customers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public List<Customers> GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> asiakkaat = context.Customers.ToList();
+            return asiakkaat;
+        }
+        [Route("{key}")]
+      
+     
+            public Customers GetSingle(string key)
+            {
+                NorthwindContext context = new NorthwindContext();
+            Customers asiakas = context.Customers.Find(key);
+                return asiakas;
+            }
+        }
+    }
